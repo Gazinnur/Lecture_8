55b34c38409e1196015b76903e5a6d1cf4b6d9a8
gazinnur <gazinnur@mail.ru>
2022-11-03 00:20:17 +0300

    add epx 010 train array

---
+++ Exp010_trainArray/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] array = { 1, 223, 3, 45, 6, 27, 84, 95, 95 };
+int n = array.Length;
+
+int find = 95;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find) {Console.WriteLine(index); break;}
+    index++;
+}
