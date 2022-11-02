b3b584a42bf8928410549dad435845417210d683
gazinnur <gazinnur@mail.ru>
2022-11-03 00:08:37 +0300

    add exp 009 intro array

---
+++ Exp009_introArray/Program.cs
@@ -0,0 +1,18 @@
+﻿// нахождение максимуума 9 переменных
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int[] array = { 10, 442, 13, 43, 5, 66, 47, 83, 90 };
+array[0] = 12;
+//Console.WriteLine(array[0]);
+
+//Console.WriteLine(max);
+int max=Max(Max(array[0],array[1],array[2]),
+            Max(array[3],array[4],array[5]),
+            Max(array[6],array[7],array[8]));
+
+Console.WriteLine(max);
